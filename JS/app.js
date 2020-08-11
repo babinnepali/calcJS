08b8cbc1a3a45004f6efcf31ce44b35cf9a0c558
babinnepali <babinnepalibtl@gmail.com>
2020-08-11 10:39:01 +0545

    Added functionality

--- JS/app.js
+++ JS/app.js
@@ -13,7 +13,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
@@ -58,11 +58,36 @@ class Calculator {
         thsi.previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('o')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return "${integerDisplay}.${decimalDigits}"
+
+        } else {
+            return integerDisplay
+        }
+
+    }
+
     updateDisplay() {
 
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.precviousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand) //not displays previous inner-text(issue)
 
+        if (this.operation != null) {
+            this.previousOperandTextElement =
+                '${this.getDisplayNumber(this.previousOperand)} ${this.operation}'
+        } else {
+            this.previousOperandTextElement = ''
+        }
 
     }
 }
@@ -95,5 +120,15 @@ operationButtons.forEach(button => {
 
 equalsButton.addEventListener('click', button => {
     calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
